a3755b899827197365e16ea105c8dc79d65e9e77
hchen <hchen229@gmail.com>
2017-03-26 20:48:36 -0700

    Update api key signature.

--- app_configs.js
+++ app_configs.js
@@ -1,8 +1,8 @@
 module.exports = {
     "SleepAiden": {
         packageName: 'im.hch.sleeprecord',
-        apiKeySignature: "5f1497592a223b5559064957b90eb2f0",
-        apkSHA256Signature: "F5:FF:1E:15:52:9D:96:84:16:A6:41:32:74:1D:A9:54:9A:C3:56:E7:CD:0D:28:DB:78:AF:C1:36:F9:36:A1:37",
+        apiKeySignature: "354649080795099af4fdd13f010fc867",
+        apkSHA256Signature: "B1:72:99:D8:C2:11:AA:71:9E:83:D7:E3:CC:03:05:94:DE:EB:20:67:2F:57:87:E3:CD:C1:8D:D0:69:45:EA:5B",
         magicWord: "HCH",
         // key: packageName + magicWord + apkSHA256Signature + apiKeySignature
     }
