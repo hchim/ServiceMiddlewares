1b54f78ec2a421f78a3f200ae47dc619e864c054
Hui Chen <hui@hch.im>
2017-06-18 09:15:41 -0700

    Update app_configs.js

--- app_configs.js
+++ app_configs.js
@@ -7,7 +7,7 @@ module.exports = {
         // key: packageName + magicWord + apkSHA256Signature + apiKeySignature
     },
     "AlphabetSong": {
-        packageName: 'com.sleepaiden.alphabetsong',
+        packageName: 'com.sleepaiden.alphebetsong',
         apiKeySignature: "87d8072f69ae49852b3486a9f2dc82c8",
         apkSHA256Signature: "B1:72:99:D8:C2:11:AA:71:9E:83:D7:E3:CC:03:05:94:DE:EB:20:67:2F:57:87:E3:CD:C1:8D:D0:69:45:EA:5B",
         magicWord: "HCH",
