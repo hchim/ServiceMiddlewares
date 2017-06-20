feb395fd66e3e8e1a5cbfb4e272fa97354ae4f78
Hui Chen <huiche@amazon.com>
2017-06-19 21:55:27 -0700

    Add configs for AlphabetSong-Amazon.

--- app_configs.js
+++ app_configs.js
@@ -12,5 +12,12 @@ module.exports = {
         apkSHA256Signature: "B1:72:99:D8:C2:11:AA:71:9E:83:D7:E3:CC:03:05:94:DE:EB:20:67:2F:57:87:E3:CD:C1:8D:D0:69:45:EA:5B",
         magicWord: "HCH",
         // key: packageName + magicWord + apkSHA256Signature + apiKeySignature
+    },
+    "AlphabetSong-Amazon": {
+        packageName: 'com.sleepaiden.alphebetsong',
+        apiKeySignature: "e9238748e4d683aa59994d08069a2226",
+        apkSHA256Signature: "10:6D:E4:D0:11:EF:86:C8:90:FC:1A:FE:9E:32:8B:88:72:BA:8F:F7:A2:82:EE:D5:8D:49:1E:6A:D6:82:6E:BC",
+        magicWord: "HCH",
+        // key: packageName + magicWord + apkSHA256Signature + apiKeySignature
     }
 }
