ee5c01949e6f66f38323bb6807e9921168d07e68
Hui Chen <huiche@amazon.com>
2017-08-13 02:36:16 -0700

    Add error handlers

--- index.js
+++ index.js
@@ -105,6 +105,56 @@ var signature_middleware = function (req, res, next) {
 };
 
 /**
+ * 404 error handler
+ * @param req
+ * @param res
+ * @param next
+ */
+var error_404_middleware = function(req, res, next) {
+    var err = new Error('404 Not Found');
+    err.status = 404;
+    next(err);
+}
+
+/**
+ * Error 500 handler
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
+var error_500_middleware_dev = function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json(utils.encodeResponseBody(req, {
+        message: err.message,
+        error: err
+    }));
+}
+
+/**
+ * Error 500 handler for production
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
+var error_500_middleware_prod = function(err, req, res, next) {
+    res.status(err.status || 500);
+    self.winston.log('error', err.message, err);
+    self.metricClient.errorMetric(self.serviceName + ':Error:' + req.method + ':' + req.url, err, function (error, jsonObj) {
+        if (error != null)
+            return res.json(utils.encodeResponseBody(req, {
+                message: 'Failed to add metric. \n' + err.message,
+                "errorCode": "INTERNAL_FAILURE"
+            }));
+        return res.json(utils.encodeResponseBody(req, {
+            "message": err.message,
+            "errorCode": "INTERNAL_FAILURE"
+        }));
+    })
+}
+
+/**
  * Exports a init function. It can be used as follows:
  *
  * var authMw = require('service-middlewares')(config);
@@ -121,9 +171,13 @@ module.exports = function (config) {
     this.redisClient = utils.createRedisClient(host, port);
     this.appConfs = sigHeper.appConfigs()
     this.metricClient = require('metricsclient')(config)
+    this.winston = utils.getWinston(config.get('env'))
 
     this.auth_middleware = auth_middleware;
     this.signature_middleware = signature_middleware;
+    this.error_404_middleware = error_404_middleware;
+    this.error_500_middleware_dev = error_500_middleware_dev;
+    this.error_500_middleware_prod = error_500_middleware_prod;
 
     return this;
 };
